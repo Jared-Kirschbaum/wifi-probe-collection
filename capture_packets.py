5e4a73ef0e99b2b2dfb540678f4ddfffe93555ce
Jared Kirschbaum <65467962+Jared-Kirschbaum@users.noreply.github.com>
2024-09-04 08:22:20 -0400

    Add files via upload

--- capture_packets.py
+++ capture_packets.py
@@ -1,6 +1,11 @@
+import sys
 from scapy.all import sniff, Dot11ProbeReq
 import json
 from azure.iot.device import IoTHubDeviceClient, Message
+from azure.iot.hub import IoTHubRegistryManager
+import os
+import uuid
+import base64
 from dotenv import load_dotenv
 import os
 
@@ -31,6 +36,60 @@ def create_payload(mac_address: str, ssid: str) -> str:
     }
     return json.dumps(event_data)
 
+# Function to generate a base64-encoded key
+def generate_base64_key() -> str:
+    
+    try:        
+        return base64.b64encode(os.urandom(32)).decode('utf-8')
+    
+    except Exception as e:
+        print(f"Failed to Generate Keys: {e}")
+
+def generate_device_id() -> str:
+
+    try:
+        return f"BOWSER-{uuid.uuid4()}"
+
+    except Exception as e:
+        print(f"Failed to Generate Device ID: {e}")
+
+def create_iot_hub_connection():
+
+    return IoTHubRegistryManager(CONNECTION_STRING)
+
+
+def register_device(device_id: str) -> None:
+    
+
+    try:
+
+        registry_manager = create_iot_hub_connection()
+
+    except Exception as e:
+
+        print(f"Failed to connect to IOT Hub: {e}")
+
+
+    try:
+
+        device_id = f"BOWSER-{uuid.uuid4()}"
+
+        registry_manager.create_device_with_sas(
+        device_id=generate_device_id,
+        primary_key=generate_base64_key(),
+        secondary_key=generate_base64_key(),
+        status="enabled"
+        )
+        
+        print(f"Created device: {device_id}")
+
+    except Exception as ex:
+        
+        if "DeviceAlreadyExists" in str(ex):
+            print(f"Device '{device_id}' already exists. Skipping.")
+        else:
+            print(f"Failed to create device '{device_id}': {ex}")
+
 def packet_handler(packet) -> None:
     """Handle each captured packet."""
     if has_probe_request(packet):
@@ -46,5 +105,12 @@ def packet_handler(packet) -> None:
         except Exception as e:
             print(f"Failed to send message: {e}")
 
-# Start capturing packets on the "panda" interface, filtering for probe request frames
-sniff(iface="panda", prn=packet_handler, filter="wlan type mgt subtype probe-req")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <interface>")
+        sys.exit(1)
+
+    interface = sys.argv[1]
+
+    # Start capturing packets on the specified interface, filtering for probe request frames
+    sniff(iface=interface, prn=packet_handler, filter="wlan type mgt subtype probe-req")
